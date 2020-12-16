0aacf91fd89892167d23040dde53aab49f4fb7b8
Knut Valen <knut.valen@icloud.com>
2020-12-16 15:57:56 +0100

    add importance

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -55,6 +55,7 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         .setStyle(bigPictureStyle)
         .setLargeIcon(eggImage)
         .addAction(R.drawable.egg_icon, applicationContext.getString(R.string.snooze), snoozePendingIntent)
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
 
     notify(NOTIFICATION_ID, builder.build())
 }
